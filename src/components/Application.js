b442f39148a6b9e4206f5196dfd01ea99083608c
Ibirn <kmcricciuti@gmail.com>
2020-09-29 13:01:29 -0700

    Add Interviewer List

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
+import DayList from "./DayList";
+import InterviewerList from "./InterviewerList";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
 
 export default function Application(props) {
+  const [getDay, setDay] = useState("Monday");
+  const [getInterviewer, setInterviewer] = useState(1)
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -12,7 +42,18 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={getDay}
+            setDay={setDay}
+            />
+          <InterviewerList
+            interviewers={interviewers}
+            interviewer={getInterviewer}
+            setInterviewer={setInterviewer}
+            />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
