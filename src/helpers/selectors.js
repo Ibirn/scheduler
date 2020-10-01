4697363bc432be10a7a7938720516f2bca0d3732
Ibirn <kmcricciuti@gmail.com>
2020-09-30 18:49:09 -0700

    Made default export

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export function getAppointmentsForDay (state, day) {
+export default function getAppointmentsForDay (state, day) {
   const output = [];
   for (const elem of state.days) {
     if(elem.name === day){
