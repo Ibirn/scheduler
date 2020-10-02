3a22381c42a7961c06d8af621e3bf802ee41bec8
Ibirn <kmcricciuti@gmail.com>
2020-10-02 15:22:18 -0700

    Add support for book/cancel interview functions

--- src/components/Application.js
+++ src/components/Application.js
@@ -14,8 +14,8 @@ export default function Application(props) {
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const dailyInterviewers = getInterviewersForDay(state, state.day);
-
   const setDay = day => setState({ ...state, day });
+
   useEffect(() => {
     const baseURL = "http://localhost:8001"
     const promOne = axios.get(`${baseURL}/api/days`)
@@ -27,6 +27,32 @@ export default function Application(props) {
       }
       )
   }, [])
+
+  function bookInterview(id, interview) {
+    const appointment = { //captures and copies save function's interview object
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    setState({...state, appointments}) //sets state as was but with new appointments data
+    return axios.put(`/api/appointments/${id}`, appointment)
+  }
+
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    }
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    }
+    setState({...state, appointments})
+    return axios.delete(`/api/appointments/${id}`, appointment)
+  }
 
   return (
     <main className="layout">
@@ -58,9 +84,12 @@ export default function Application(props) {
           return (
             <Appointment
               key={appointment.id}
+              id={appointment.id}
               interviewer={dailyInterviewers}
               interview={interview}
               time={appointment.time}
+              bookInterview={bookInterview}
+              cancelInterview={cancelInterview}
             />)
         })}
         <Appointment key="last" time="5pm" />
