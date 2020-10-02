377492a6f29311f420a13d55847dc98cba929624
Ibirn <kmcricciuti@gmail.com>
2020-10-01 18:13:11 -0700

    Remove comments

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 export default function useVisualMode(initMode) {
   const [mode, setMode] = useState(initMode)
   const [history, setHistory] = useState([initMode])
-  console.log("MODE: ", mode)
 
   const transition = (newMode, replace = false) => {
     if (replace) {  //if replace is true, change mode, don't add to history so that back calls the last time replace was not true.
@@ -12,15 +11,14 @@ export default function useVisualMode(initMode) {
       setMode(newMode)
       setHistory([...history, newMode]) //use spread because you want to copy everything that already exists in history AND newMode
     }
-    console.log("TRANSITIONH: ", history)
   }
 
   const back = () => {
     if (history.length !== 0) {
       history.pop() //Remove last hist item
       setMode(history[history.length - 1])//set mode to new last hist item
+    }
   }
-    } 
 
   return { mode, transition, back }
 }
