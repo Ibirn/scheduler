c67f4a4fc4f899663c35dee22e3a71aa3e185f44
Ibirn <kmcricciuti@gmail.com>
2020-10-06 08:27:58 -0700

    Linting

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import "components/InterviewerList.scss"
-import InterviewerListItem from "components/InterviewerListItem"
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
+import PropTypes from "prop-types";
 
 export default function InterviewerList(props) {
-  //console.log("IL: ",props)
+  InterviewerList.propTypes = {
+    interviewers: PropTypes.array.isRequired,
+  };
+  // console.log("PROPSINT: ", props.interviewers)
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
-        {props.interviewers.map(person => 
-          <InterviewerListItem 
+        {props.interviewers.map((person) => (
+          <InterviewerListItem
             key={person.id}
             avatar={person.avatar}
             name={person.name}
             onChange={() => props.onChange(person.id)}
             selected={props.value === person.id}
           />
-        )}
+        ))}
       </ul>
     </section>
-  )
-};
+  );
+}
