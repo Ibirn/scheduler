a96dab7dd41e64dcc9a38973e9de7112e9dc9a63
Ibirn <kmcricciuti@gmail.com>
2020-10-07 08:58:01 -0700

    Linting

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,28 +1,6 @@
 import React from "react";
-
 import { render, cleanup } from "@testing-library/react";
-
 import Appointment from "components/Appointment/index";
-
-const propData = {
-  "id": 1,
-  "interviewers": [
-    {"avatar": "https://i.imgur.com/LpaY82x.png", "id": 1, "name": "Sylvia Palmer"},
-    {"avatar": "https://i.imgur.com/T2WwVfS.png", "id": 3, "name": "Mildred Nazir"},
-    {"avatar": "https://i.imgur.com/twYrpay.jpg", "id": 5, "name": "Sven Jones"}
-  ],
-  "interview": {
-    "student": "Alice Liddell",
-    "interviewer": {
-      "id": 1,
-      "name": "Sylvia Palmer",
-      "avatar": "https://i.imgur.com/LpaY82x.png"
-    }
-  },
-  "time": "12pm",
-  "bookInterview": "ƒ bookInterview() {}",
-  "cancelInterview": "ƒ cancelInterview() {}"
-}
 
 afterEach(cleanup);
 
--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -9,15 +9,13 @@ afterEach(cleanup);
 
 describe("Form", () => {
   const interviewersData = [
-      {avatar: "https://i.imgur.com/LpaY82x.png", id: 1, name: "Sylvia Palmer"},
-      {"avatar": "https://i.imgur.com/T2WwVfS.png", "id": 3, "name": "Mildred Nazir"},
-      {"avatar": "https://i.imgur.com/twYrpay.jpg", "id": 5, "name": "Sven Jones"}
-    ]
+    { avatar: "https://i.imgur.com/LpaY82x.png", id: 1, name: "Sylvia Palmer" },
+    { avatar: "https://i.imgur.com/T2WwVfS.png", id: 3, name: "Mildred Nazir" },
+    { avatar: "https://i.imgur.com/twYrpay.jpg", id: 5, name: "Sven Jones" },
+  ];
 
   it("renders without crashing", () => {
-    const { getByPlaceholderText } = render(
-      <Form interviewers={interviewersData} />
-    );
+    render(<Form interviewers={interviewersData} />);
   });
 
   it("renders without student name if not provided", () => {
@@ -35,38 +33,36 @@ describe("Form", () => {
   });
 
   it("validates that the student name is not blank", () => {
-    const onSave = jest.fn()
+    const onSave = jest.fn();
     const { getByText } = render(
       <Form interviewers={interviewersData} onSave={onSave} />
     );
     fireEvent.click(getByText("Save"));
 
-    /* 1. validation is shown */
     expect(getByText(/The name cannot be left blank./i)).toBeInTheDocument();
-  
-    /* 2. onSave is not called */
+
     expect(onSave).not.toHaveBeenCalled();
   });
-  
+
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
     const { getByText, getByPlaceholderText, queryByText } = render(
       <Form interviewers={interviewersData} onSave={onSave} />
     );
-  
+
     fireEvent.click(getByText("Save"));
-  
+
     expect(getByText(/The name cannot be left blank./i)).toBeInTheDocument();
     expect(onSave).not.toHaveBeenCalled();
-  
+
     fireEvent.change(getByPlaceholderText("Enter Student Name"), {
-      target: { value: "Lydia Miller-Jones" }
+      target: { value: "Lydia Miller-Jones" },
     });
-  
+
     fireEvent.click(getByText("Save"));
-  
+
     expect(queryByText(/The name cannot be left blank./i)).toBeNull();
-  
+
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
@@ -81,19 +77,19 @@ describe("Form", () => {
         onCancel={onCancel}
       />
     );
-  
+
     fireEvent.click(getByText("Save"));
-  
+
     fireEvent.change(getByPlaceholderText("Enter Student Name"), {
-      target: { value: "Lydia Miller-Jones" }
+      target: { value: "Lydia Miller-Jones" },
     });
-  
+
     fireEvent.click(getByText("Cancel"));
-  
+
     expect(queryByText(/The name cannot be left blank./i)).toBeNull();
-  
+
     expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
-  
+
     expect(onCancel).toHaveBeenCalledTimes(1);
   });
 });
